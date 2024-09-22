236741c5fce3f1f7a6ccca12854c834fdde32461
FedotovAleksndrVl <FedotovAleksndrVl@gmail.com>
2024-09-22 11:53:07 +0300

    Completed

--- src/main/java/app/model/Cat.java
+++ src/main/java/app/model/Cat.java
@@ -1,9 +1,18 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("cat")
 public class Cat extends Animal {
+
+    private Animal cat;
+
+//    @Autowired
+//    public Cat(Animal cat) {
+//        this.cat = cat;
+//    }
+
     @Override
     public String toString() {
         return "Im a Cat";
